d7d17d6e94a8d6bfbbd92b349cadb2de403eafff
Andres Huerta <and.23108@gmail.com>
2022-03-28 06:47:42 +0000

    Calculator for peer evaluation finished

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    let p = document.getElementById("principal").value;
+    let rate=document.getElementById("rate").value;
+    let years=document.getElementById("years").value;
+    const date = new Date();
+    let finalDate=parseInt(years)+date.getFullYear();
+    let amount=p*(rate/100)*parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit "+p+",<br>at an interest rate of "+rate+"%.<br>You will receive an amount of "+amount.toFixed(2)+",<br>in the year "+finalDate;
+
+
+    return false;
 }
         
